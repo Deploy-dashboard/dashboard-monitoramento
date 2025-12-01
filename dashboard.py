dbae2ecfe251c8a709beb6b0218a1fae1075b6ec
lukas <lukasbot09@gmail.com>
2025-12-01 14:28:06 -0300

    Otimização da escolha de programas e subprogramas, odenação das seleções e tratamento dos erros. Adição de novas funcionalidades e gráficos

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from client import Client
 import streamlit as st
 import pandas as pd
-import requests
+import requests, re
 import plotly.graph_objects as go
 
 if 'logged_in' not in st.session_state:
@@ -38,21 +38,10 @@ def login_page():
             
         st.error("Usuário ou senha incorretos.")
 
-programas = {"null" : ["null"],
-            234 : [2026, 2087, "null"],
-            220 : [2050, "null"],
-            130 : [2070, "null"],
-            149 : [2075, "null"],
-            194 : [2077, "null"],
-            208 : [2082, "null"],
-            197 : [2085, "null"],
-            234 : [2026, 2087, "null"],
-            129 : [2132, "null"]
-            }
-
 subprogramas = ["Todos", 
                 "2026 - MG BELO HORIZONTE - 3ª AV. SOMATIVA 2025 (SIMULADO)", 
-                "2050 - BR CAEd - PPGP 2025", 
+                "2050 - BR CAEd - PPGP 2025",
+                "2111 - RJ RIO DE JANEIRO (MUNICÍPIO) - 4ª AV. FORMATIVA 2025 (ADR 4)", 
                 "2070 - GO GOIÁS - AV. SOMATIVA EF EM 2025 (SAEGO)", 
                 "2075 - CE CEARÁ - AV. SOMATIVA EM 2025 (SPAECE EM)", 
                 "2082 - TO TOCANTINS - 1ª AV. SOMATIVA 2025 (SIMULADO)", 
@@ -64,6 +53,22 @@ subprogramas = ["Todos",
                 "2104 - BR CAEd - PRÉ-TESTE 2025 - LOTE 04 (BANCO)"
                 ]
 
+subprogramas = subprogramas[:1] + sorted(subprogramas[1:])
+
+programas = {"Todos" : ["Todos"],
+            "234 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DO MUNICÍPIO DE BELO HORIZONTE" : ["2026 - MG BELO HORIZONTE - 3ª AV. SOMATIVA 2025 (SIMULADO)", "2087 - MG BELO HORIZONTE - 5ª AV. FORMATIVA 2025 (NOVEMBRO)"],
+            "220 - PROGRAMA DE PÓS-GRADUAÇÃO PROFISSIONAL CAEd 2022" : ["2050 - BR CAEd - PPGP 2025"],
+            "130 - AVALIAÇÃO E MONITORAMENTO DA EDUCAÇÃO PÚBLICA DE GOIÁS 2019/2020" : ["2070 - GO GOIÁS - AV. SOMATIVA EF EM 2025 (SAEGO)"],
+            "149 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DO CEARÁ 2019" : ["2075 - CE CEARÁ - AV. SOMATIVA EM 2025 (SPAECE EM)"],
+            "194 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DO MUNICÍPIO DO RIO DE JANEIRO 2021" : ["2111 - RJ RIO DE JANEIRO (MUNICÍPIO) - 4ª AV. FORMATIVA 2025 (ADR 4)"],
+            "208 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DE TOCANTINS 2021" : ["2082 - TO TOCANTINS - 1ª AV. SOMATIVA 2025 (SIMULADO)"],
+            "197 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DO MATO GROSSO 2021" : ["2085 - MT MATO GROSSO - AV. SOMATIVA 2025 EF EM (AVALIA MT)"],
+            "129 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DO PIAUÍ 2019" : ["2132 - PI PIAUÍ - AV. SOMATIVA 2025 EF EM (SAEPI)"],
+            "212 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DE RECIFE 2022" : ["2091 - PE RECIFE - 3ª AV. FORMATIVA 2025"],
+            "244 - PROGRAMA DE AVALIAÇÃO E MONITORAMENTO DE FLORIANÓPOLIS" : ["2101 - SC FLORIANÓPOLIS - 2ª AV. SOMATIVA 2025"],
+            "213 - CONSTRUÇÃO E PRÉ-TESTAGEM DOS ITENS" : ["2104 - BR CAEd - PRÉ-TESTE 2025 - LOTE 04 (BANCO)"],
+            }
+
 def num_sp(sp):
     if sp != "Todos":
         num = sp[0:4]
@@ -71,10 +76,10 @@ def num_sp(sp):
         return num
     return "null"
 
-def atualiza_id(id):
-
-    if id == "null":
+def atualiza_id(prog):
+    if prog == "Todos":
         return "null"
+    id = int(prog[0:3])
     if id == 234:
         return 250
     elif id == 220:
@@ -83,13 +88,18 @@ def atualiza_id(id):
         return 124
     elif id == 149:
         return 143
+    elif id == 194:
+        return 198
     elif id == 208:
         return 212
     elif id == 197:
         return 201
-    elif id == 234:
-        return 250
-    return 123
+    elif id == 129:
+        return 123
+    elif id == 244:
+        id = 260
+    return 217
+
 
 
 def report_tab1():
@@ -102,12 +112,18 @@ def report_tab1():
 
     excel = BytesIO(response.content)
     df = pd.DataFrame(pd.read_excel(excel))
+    df.insert(loc=3, column="% de registros digitalizados", value=((df["Total de registros digitalizados"] / df["Total de registros previstos"])*100).round(2))
+
+    mapa_sub = { s.split(" - ")[0]: " - ".join(s.split(" - ")[1:]) for s in subprogramas if s != "Todos"}
+    df.insert(1, "Nome subprograma", df["Cód. subprograma"].astype(str).map(mapa_sub))
+
     st.markdown("**Tabela:**")
     st.dataframe(df, hide_index=True)
     
     if sp == "null":
         labels = df["Cód. subprograma"].astype(str)  
         decodificados = df["% de registros processados"]
+        digitalizados = df["% de registros digitalizados"]
 
         fig = go.Figure()
 
@@ -116,12 +132,21 @@ def report_tab1():
             y=decodificados,
             name="Decodificados",
             marker_color='#4169E1',
-            offsetgroup=0
+            offsetgroup=1
+        ))
+
+        fig.add_trace(go.Bar(
+            x=labels,
+            y=digitalizados,
+            name="Digitalizados",
+            marker_color='lightblue',
+            offsetgroup=2
         ))
 
 
+
         fig.update_layout(
-            title="Comparativo de registros processados: ",
+            title="Comparativo de registros digitalizados e processados: ",
             xaxis_title="Código do Subprograma",
             yaxis_title="Registros processados (%)",
             barmode='group',  
@@ -151,7 +176,8 @@ def report_tab2():
     df = pd.DataFrame(pd.read_excel(excel))
     
     instrumentos = df['Instrumento'].unique().tolist()
-    instrumentos.append("Todos")
+    instrumentos = sorted(instrumentos)
+    instrumentos.insert(0, "Todos")
 
     select_sp = st.selectbox("Suprograma",options=subprogramas, key="sp_tab2")
     sp = num_sp(select_sp)
@@ -303,22 +329,26 @@ def report_tab3():
 
     st.subheader("Selecione programa, subprograma e solicitação:")
     verif = df["Verificação"].unique().tolist()
+    verif.remove("Subtotal")
+    del verif[-1]
     entrada_prog = st.selectbox("Programa", options=list(programas.keys()), key="prog_tab3")
+
     prog = atualiza_id(entrada_prog)
 
     subprogramas = programas[entrada_prog]
     sp = st.selectbox("Suprograma",options=subprogramas, key="sp_tab3")
+    subprog = num_sp(sp)
 
     solicitacao = str(st.selectbox("Solicitacao", options=verif))
-    if solicitacao == "nan":
-        solicitacao = "null"
     solicitacao = solicitacao.replace(" ", "%20")
 
-    url = f"http://10.0.10.22:41112/gw/reports/generate_report_xls/CAED7029-2307:2025/9/ID_FONTE_DADO={prog}&CD_PROGRAMA={sp}&DC_SOLICITACAO={solicitacao}"
+    url = f"http://10.0.10.22:41112/gw/reports/generate_report_xls/CAED7029-2307:2025/9/ID_FONTE_DADO={prog}&CD_PROGRAMA={subprog}&DC_SOLICITACAO={solicitacao}"
 
     resp = requests.get(url)
     excel = BytesIO(resp.content)
     table = pd.DataFrame(pd.read_excel(excel))
+    table = table[table["Verificação"] != "Subtotal"]
+    table = table[table["Cód. subprograma"] != "Total"]
     st.markdown("**Tabela:**")
     st.dataframe(table, hide_index=True)
 
@@ -344,13 +374,15 @@ def dashboard():
         report_tab3()
 
 
+# def main():
+#   if st.session_state['logged_in']:
+#       dashboard()
+#   else:
+#       login_page()
 
 
 # if __name__ == "__main__":
+#   main()
 
-#     if st.session_state['logged_in']:
-#         dashboard()
-#     else:
-#         login_page()
 
 dashboard()
